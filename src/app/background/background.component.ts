4a90ec236a68aa2236faca5774f920665d9eea9b
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2025-01-01 03:09:46 -0300

    Refactor: Create Background Component Logic

---
+++ src/app/background/background.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-background',
+  templateUrl: './background.component.html',
+  styleUrl: './background.component.css',
+  standalone: false,
+})
+export class BackgroundComponent {}
