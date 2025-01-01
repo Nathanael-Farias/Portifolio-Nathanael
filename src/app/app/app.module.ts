dbbad821e1f20d8d7d7f10f062883148f8adce9d
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2025-01-01 03:05:09 -0300

    Feat: Set Up AppModule with All Required Components

---
+++ src/app/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { HamburgerMenuComponent } from '../hamburger-menu/hamburger-menu.component';
+import { BackgroundComponent } from '../background/background.component';
+import { ResumeComponent } from '../resume/resume.component';
+import { SkillsComponent } from '../skills/skills.component';
+import { ProjectsResumeComponent } from '../projects-resume/projects-resume.component';
+import { StudiesComponent } from '../studies/studies.component';
+import { FooterComponent } from '../footer/footer.component';
+import { ProjectCardComponent } from '../project-card/project-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HamburgerMenuComponent,
+    BackgroundComponent,
+    ResumeComponent,
+    SkillsComponent,
+    ProjectsResumeComponent,
+    StudiesComponent,
+    ProjectCardComponent,
+    FooterComponent,
+  ],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
