58b3f032714c90051f3454bb40487e981dfdfa87
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2025-01-01 03:12:44 -0300

    Refactor: Create Studies Component Logic

---
+++ src/app/studies/studies.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-studies',
+  templateUrl: './studies.component.html',
+  styleUrl: './studies.component.css',
+  standalone: false,
+})
+export class StudiesComponent {}
