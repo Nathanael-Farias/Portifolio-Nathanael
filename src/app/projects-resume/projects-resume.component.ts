46b19e8b5d141aff1b1594438faac5cf281f131c
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2025-01-01 03:19:52 -0300

    Feat: Create Projects Resume Component Logic

---
+++ src/app/projects-resume/projects-resume.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects-resume',
+
+  templateUrl: './projects-resume.component.html',
+  styleUrl: './projects-resume.component.css',
+  standalone: false,
+})
+export class ProjectsResumeComponent {}
