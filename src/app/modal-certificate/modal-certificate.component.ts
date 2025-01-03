fbef37631be52b17a14b186b072e0b0334c41a0e
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2025-01-03 18:35:43 -0300

    Refactor modal opening to include correct animations

--- src/app/modal-certificate/modal-certificate.component.ts
+++ src/app/modal-certificate/modal-certificate.component.ts
@@ -1,4 +1,4 @@
-import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { Component, TemplateRef, ViewChild, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-modal-certificate',
@@ -13,16 +13,35 @@ export class ModalCertificateComponent {
   @ViewChild('certificateCardTemplate', { static: true })
   dynamicTemplate!: TemplateRef<any>;
 
+  constructor(private renderer: Renderer2) {}
+
   openModal() {
     this.isVisible = true;
     this.isFadingOut = false;
+
+    this.renderer.setStyle(document.body, 'overflow', 'hidden');
   }
 
   closeModal() {
     this.isFadingOut = true;
+  
+   
+    const backdrop = document.querySelector('.modal-backdrop');
+    if (backdrop) {
+      this.renderer.addClass(backdrop, 'fade-out');
+    }
+  
+    
     setTimeout(() => {
-      this.isVisible = false;
+      this.isVisible = false; 
       this.isFadingOut = false;
-    }, 1000);
+      this.renderer.removeStyle(document.body, 'overflow');
+  
+      
+      if (backdrop) {
+        this.renderer.removeClass(backdrop, 'fade-out');
+      }
+    }, 500); 
   }
+  
 }
